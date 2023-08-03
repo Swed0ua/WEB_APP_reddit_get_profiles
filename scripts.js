df9f5321687154747f5e8b5012f7824f0c9eb3c8
Swed0ua <shmagala2001@gmail.com>
2023-08-03 13:32:39 +0300

    add block select with options types

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 let tg = window.Telegram.WebApp;
 let btn_save = document.querySelector('.btn__save')
 let btns_cancel = document.querySelectorAll('.btn__cancel')
-let link__input, stop_words, max_posts
+let link__input, stop_words, max_posts, type_work
 const init_params = () => {
     link__input = document.querySelector('#link__input').value,
     stop_words = document.querySelector('#stop_words').value,
     max_posts = document.querySelector('#max_posts').value
+    type_work = document.querySelector('#type_work').value
 }
 
 init_params()
@@ -20,7 +21,8 @@ const give_params = () => {
     tg.sendData(JSON.stringify({
         "link" : link__input,
         'stop_words': stop_words,
-        'max_posts': max_posts
+        'max_posts': max_posts,
+        'type_work':type_work
     }))
     cancel_app()
 }
