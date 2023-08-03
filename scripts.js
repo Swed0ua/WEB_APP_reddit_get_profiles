4ec6f9819fe6aa84f207b015906f187b15720818
Swed0ua <shmagala2001@gmail.com>
2023-08-03 12:38:17 +0300

    WEB_APP TG for `reddit_bor_get_profiles`

---
+++ scripts.js
@@ -0,0 +1,33 @@
+let tg = window.Telegram.WebApp;
+let btn_save = document.querySelector('.btn__save')
+let btns_cancel = document.querySelectorAll('.btn__cancel')
+let link__input, stop_words, max_posts
+const init_params = () => {
+    link__input = document.querySelector('#link__input').value,
+    stop_words = document.querySelector('#stop_words').value,
+    max_posts = document.querySelector('#max_posts').value
+}
+
+init_params()
+
+
+const cancel_app = () => {
+    tg.close()
+}
+
+const give_params = () => {
+    init_params()
+    tg.sendData(JSON.stringify({
+        "link" : link__input,
+        'stop_words': stop_words,
+        'max_posts': max_posts
+    }))
+    cancel_app()
+}
+
+
+btn_save.addEventListener('click', give_params)
+
+btns_cancel.forEach(btn_cancel => {
+    btn_cancel.addEventListener('click', cancel_app) 
+});
